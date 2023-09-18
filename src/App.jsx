d9a0a324a1a470529e76fb64779aaf743d90e283
mifalia <mifarivonjisoa@gmail.com>
2023-09-18 23:05:25 +0300

    Set up and config tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='text-red-900'>
+      <p className='text-3xl'>Hello Everyone !!!</p>
+    </div>
+  );
+}
+
+export default App;
